6a9f34306ba30df8925c7596743373a3e86a9f1b
eerga <63570565+eerga@users.noreply.github.com>
2026-07-15 18:32:34 +0000

    adding the saving items

--- chapter_5/assistant.py
+++ chapter_5/assistant.py
@@ -5,6 +5,7 @@ from openai import OpenAI
 
 from ingest import load_faq_data, build_index
 from metrics import RAGWithMetrics
+from db_save import save_conversation
 
 def create_assistant():
     load_dotenv()
@@ -26,3 +27,4 @@ if __name__ == "__main__":
 
     answer = assistant.rag(query)
     print(answer)
+    save_conversation(assistant.last_call, query, "llm-zoomcamp")
